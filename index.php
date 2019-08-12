553ee8b3092820b794e30136cadd31df1fc3ef6a
Ihor <igorboiar11@gmail.com>
2019-08-12 19:04:40 +0300

    Поменял с руского на английский ключи в масиве

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ $user_name = 'Ihor'; // укажите здесь ваше имя
                 <a class="promo__link" href="#"><?= $item ?></a>
             </li>
              <?php endforeach; ?>
-            
+
         </ul>
     </section>
     <section class="lots">
@@ -75,55 +75,56 @@ $user_name = 'Ihor'; // укажите здесь ваше имя
             <?php
             $items = [
             [
-            'Название' => '2014 Rossignol District Snowboard',
-            'Категория' => 'Доски и лыжи',
-            'Цена' => '10999',
-            'URL картинки' => 'img/lot-1.jpg',
+            'title' => '2014 Rossignol District Snowboard',
+            'category' => 'Доски и лыжи',
+            'price' => '10999',
+            'URL img' => 'img/lot-1.jpg',
             ],
             [
-            'Название' => 'DC Ply Mens 2016/2017 Snowboard',
-            'Категория' => 'Доски и лыжи',
-            'Цена' => '159999',
-            'URL картинки' => 'img/lot-2.jpg',
+            'title' => 'DC Ply Mens 2016/2017 Snowboard',
+            'category' => 'Доски и лыжи',
+            'price' => '159999',
+            'URL img' => 'img/lot-2.jpg',
              ],
              [
-            'Название' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-            'Категория' => 'Крепления',
-            'Цена' => '8000',
-            'URL картинки' => 'img/lot-3.jpg',
+            'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+            'category' => 'Крепления',
+            'price' => '8000',
+            'URL img' => 'img/lot-3.jpg',
             ],
             [
-            'Название' => 'Ботинки для сноуборда DC Mutiny Charocal',
-            'Категория' => 'Ботинки',
-            'Цена' => '10999',
-            'URL картинки' => 'img/lot-4.jpg',
+            'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+            'category' => 'Ботинки',
+            'price' => '10999',
+            'URL img' => 'img/lot-4.jpg',
             ],
             [
-            'Название' => 'Куртка для сноуборда DC Mutiny Charocal',
-            'Категория' => 'Одежда',
-            'Цена' => '7500',
-            'URL картинки' => 'img/lot-5.jpg',
+            'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+            'category' => 'Одежда',
+            'price' => '7500',
+            'URL img' => 'img/lot-5.jpg',
             ],
             [
-            'Название' => 'Маска Oakley Canopy',
-            'Категория' => 'Разное',
-            'Цена' => '5400',
-            'URL картинки' => 'img/lot-6.jpg',
+            'title' => 'Маска Oakley Canopy',
+            'category' => 'Разное',
+            'price' => '5400',
+            'URL img' => 'img/lot-6.jpg',
             ],
         ];
             ?>
             <?php foreach ($items as $item) : ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $item['URL картинки']; ?>" width="350" height="260" alt="<?= $item['Название']; ?>">
+                    <img src="" width="350" height="260" alt="">
+                    <img src="<?= $item['URL img']; ?>" width="350" height="260" alt="<?= $item['title']; ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $item['Категория']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['Название']; ?></a></h3>
+                    <span class="lot__category"><?= $item['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['title']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= $item['Цена']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $item['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -131,7 +132,7 @@ $user_name = 'Ihor'; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
-             <?php endforeach; ?>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
