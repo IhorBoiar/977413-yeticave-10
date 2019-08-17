dce08fdd8c32d6ad3e36b4e6b3a5e8d0a457b5f0
Ihor <igorboiar11@gmail.com>
2019-08-17 19:11:04 +0300

    Добавил защиту от XSS

--- templates/main.php
+++ templates/main.php
@@ -20,11 +20,11 @@
             <li class="lots__item lot">
                 <div class="lot__image">
                     <img src="" width="350" height="260" alt="">
-                    <img src="<?= $item['url_img']; ?>" width="350" height="260" alt="<?= $item['title']; ?>">
+                    <img src="<?= $item['url_img']; ?>" width="350" height="260" alt="<?= htmlspecialchars($item['title']); ?>">
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?= $item['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['title']; ?></a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($item['title']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
